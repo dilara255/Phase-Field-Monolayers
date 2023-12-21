abf00c67e18f7ec805b87b5009f95a8c99655bec
eu <en.gustavo@gmail.com>
2023-12-21 00:41:08 -0300

    v0.5.0 #1 - Bat to run a sequence of simulations with no extra input (and log)

--- controlScripts/runBatch.bat
+++ controlScripts/runBatch.bat
@@ -1,5 +1,8 @@
 @echo off
 
-python runner.py
+REM You can run a sequence of runners like this, no input required
+REM You can also create copies of this with different runners, for parallel processing : p
+
+echo.|python runner.py > logRunner.txt
 
 timeout /t -1
